5faa2743da0b0722f9efe5c8439fcf445cc448ea
dadakadam843-star <dadakadam843@gmail.com>
2025-12-05 02:39:33 +0530

    Initialize Firebase and set up authentication
    Added Firebase configuration and authentication functions.

--- firebase.js
+++ firebase.js
@@ -1,1 +1,25 @@
-// Firebase config placeholder
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_BUCKET",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
+export {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut
+};
