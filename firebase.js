abc9abbedeba787ec838924d3df01a5c2172e99a
dadakadam843-star <dadakadam843@gmail.com>
2025-12-05 02:44:59 +0530

    Update Firebase configuration with new credentials

--- firebase.js
+++ firebase.js
@@ -7,12 +7,13 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDW9Au99PFKyMz7kBKypIKPIOex_ZkJ3uY",
+  authDomain: "public-working.firebaseapp.com",
+  projectId: "public-working",
+  storageBucket: "public-working.firebasestorage.app",
+  messagingSenderId: "266169229854",
+  appId: "1:266169229854:web:300668fcf823e6d714840f",
+  measurementId: "G-L6CJBWYPXN"
 };
 
 const app = initializeApp(firebaseConfig);
